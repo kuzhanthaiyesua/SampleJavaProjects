599e12152a0a3f5a9fc7bc8cec111649896dbb22
kuzhanthaiyesua <kuzhanthaiyesua@gmail.com>
2021-07-18 10:50:14 +0530

    Adding this project newly

---
+++ src/SampleJava.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SampleJava {
+
+    public static void main(String[] args) {
+        sampleList();
+    }
+
+    public static void sampleList(){
+        ArrayList list = new ArrayList();
+        list.add("Testing");
+        list.add("Kuzhanthaiyesu");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
