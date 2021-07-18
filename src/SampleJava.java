e8f74860b3f0221ae209dbe0412f249cecc46077
kuzhanthaiyesua <kuzhanthaiyesua@gmail.com>
2021-07-18 11:08:11 +0530

    new method created

--- src/SampleJava.java
+++ src/SampleJava.java
@@ -5,9 +5,19 @@ public class SampleJava {
 
     public static void main(String[] args) {
         sampleList();
+        sampleList1();
     }
 
     public static void sampleList(){
+        ArrayList list = new ArrayList();
+        list.add("Testing");
+        list.add("Kuzhanthaiyesu");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+
+    public static void sampleList1(){
         ArrayList list = new ArrayList();
         list.add("Testing");
         list.add("Kuzhanthaiyesu");
